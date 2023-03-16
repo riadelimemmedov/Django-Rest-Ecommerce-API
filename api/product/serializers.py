c772b36105e1867c8911a14b45f0238a167b2ad9
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-03-16 09:57:45 +0400

    Add serializer for product app

---
+++ api/product/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from .models import *
+
+
+#!CategorySerializer
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+#!BrandSerializer
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = "__all__"
+
+
+#!ProductSerializer
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
