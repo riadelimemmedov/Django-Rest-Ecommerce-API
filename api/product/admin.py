d259f27981beb0d776d03884c8935a2e0d442978
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-03-16 09:57:15 +0400

    Add model to admin area

--- api/product/admin.py
+++ api/product/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . import models as m
 
 # Register your models here.
+
+
+#!CategoryAdmin
+@admin.register(m.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = m.Category
+
+
+#!BrandAdmin
+@admin.register(m.Brand)
+class BrandAdmin(admin.ModelAdmin):
+    model = m.Brand
+
+
+#!ProductAdmin
+@admin.register(m.Product)
+class ProductAdmin(admin.ModelAdmin):
+    model = m.Product
