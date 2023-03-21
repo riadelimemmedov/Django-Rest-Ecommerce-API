59d6c94472da83f7310f1270065b975cb20bd77d
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-03-21 16:14:23 +0400

    Create url endpoint for views

--- api/product/urls.py
+++ api/product/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 app_name = "product_api"
 urlpatterns = [
-    path("category/", CategoryViewSet.as_view({"get": "list"}), name="category-api")
+    path("category/", CategoryViewSet.as_view({"get": "list"}), name="category-api"),
+    path("brand/", BrandViewSet.as_view({"get": "list"}), name="brand-api"),
+    path("product/", ProductViewSet.as_view({"get": "list"}), name="product-api"),
 ]
