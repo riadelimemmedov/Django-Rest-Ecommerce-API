cf3a95b96180bd20208875502cfaf5bbe597c64f
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-03-21 20:52:53 +0400

    Create API cliend request method

--- config/tests/conftest.py
+++ config/tests/conftest.py
@@ -1,8 +1,24 @@
+# RestFramework Function And Module
+from rest_framework.test import APIClient
+
+
+# Pytest
+import pytest
 from pytest_factoryboy import register
 
-from .factories import CategoryFactory, BrandFactory
 
+# Factories
+from .factories import CategoryFactory, BrandFactory, ProductFactory
+
+
+# *Register TestModelFactory
 # if you want call this Factory in the test file you need to declare  bottom_line format = category_factory
 register(CategoryFactory)
 register(BrandFactory)
-# register(ProductFactory)
+register(ProductFactory)
+
+
+#!api_client
+@pytest.fixture  # Fixtures are functions,which will run before each test function to which it is applied.Fixtures are used to feed some data to the tests such as database connections,URLs to test and some sort of input data.
+def api_client():
+    return APIClient
