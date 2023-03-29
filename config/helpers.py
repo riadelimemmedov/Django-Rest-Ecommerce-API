9626f1eb27b833d91dc9dab31c7b6acb279de9c3
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-03-29 09:32:59 +0400

    Add returnFlugFormat for dynamic url

--- config/helpers.py
+++ config/helpers.py
@@ -1,8 +1,16 @@
 #!Python modules
 import uuid
 
+# Django modules and function
+from django.utils.text import slugify
+
 
 #!random_code
 def random_code() -> str:
     generated_number = str(uuid.uuid4())[:12].replace("-", "").upper()
     return generated_number
+
+
+#!returnFlugFormats
+def returnFlugFormat(title):
+    return slugify(title)
