dbc7e2999aedc0d87d4da434e6fa0cfc8467293b
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-03-29 09:30:26 +0400

    Add to_representation

--- api/product/serializers.py
+++ api/product/serializers.py
@@ -8,32 +8,54 @@ from .models import *
 
 #!CategorySerializer
 class CategorySerializer(serializers.ModelSerializer):
+    parent = serializers.CharField(source="parent.name", read_only=True)
+
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = ["name", "parent"]
 
 
 #!BrandSerializer
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
-        fields = "__all__"
-
-
-#!ProductSerializer
-class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
-    brand = BrandSerializer()
-
-    class Meta:
-        model = Product
-        fields = "__all__"
+        exclude = ["id"]
 
 
 #!ProductLineSerializer
 class ProductLineSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
+    # product = ProductSerializer()
 
     class Meta:
         model = ProductLine
-        fields = "__all__"
+        exclude = ["id"]
+
+
+#!ProductSerializer
+class ProductSerializer(serializers.ModelSerializer):
+    brand_name = serializers.CharField(source="brand.name")
+    category = CategorySerializer()
+    products = ProductLineSerializer(many=True)
+    # products = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            "name",
+            "brand_name",
+            "is_active",
+            "pr_slug",
+            "description",
+            "category",
+            "products",
+        ]
+
+    # def to_representation(self, instance):
+    #     data = super().to_representation(instance)
+    #     product_price = []
+    #     for i in range(0, len(data["products"])):
+    #         product_price.append(
+    #             str(ProductLine.objects.get(pk=data["products"][i]).price)
+    #         )
+    #     data["products"] = product_price
+    #     return data
